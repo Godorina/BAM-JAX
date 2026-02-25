a6b7862e9fe301106d76fca6789f74a0843d62bc
Hyun Gyu Park <72333761+Godorina@users.noreply.github.com>
2026-02-26 01:46:21 +0900

    Update run.sh

--- bam/examples/training/eval/run.sh
+++ bam/examples/training/eval/run.sh
@@ -5,6 +5,7 @@ conda activate bam_jax_nequip
 export CUDA_VISIBLE_DEVICES=2
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
 export TF_GPU_ALLOCATOR=cuda_malloc_async
+export PYTHONUNBUFFERED=1
 
 CONFIG=${1:-input.json}
 
